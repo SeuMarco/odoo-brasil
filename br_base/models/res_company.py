aa9c86b8994991551bd4a8d04a702ca57d61eae4
Marco Aurélio <magraziadei@gmail.com>
2020-01-21 05:47:57 -0300

    In version 13.0 all methods of the models are multi-record by default. We need to adapt the code to the behavior change. Remove unused l10n.br Abstract model. Remove view_type tag on action window XML definition. It's now always form (tree is not supported since 11.0 any way). Apply 2to3 script Adds *.bak [2to3 backup files] to .gitignore and more ...

--- br_base/models/res_company.py
+++ br_base/models/res_company.py
@@ -25,130 +25,142 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     def _get_address_data(self):
-        self.city_id = self.partner_id.city_id
-        self.district = self.partner_id.district
-        self.number = self.partner_id.number
+        for rec in self:
+            rec.city_id = rec.partner_id.city_id
+            rec.district = rec.partner_id.district
+            rec.number = rec.partner_id.number
 
     def _get_br_data(self):
         """ Read the l10n_br specific functional fields. """
-        self.legal_name = self.partner_id.legal_name
-        self.cnpj_cpf = self.partner_id.cnpj_cpf
-        self.inscr_est = self.partner_id.inscr_est
-        self.inscr_mun = self.partner_id.inscr_mun
-        self.suframa = self.partner_id.suframa
+        for rec in self:
+            rec.legal_name = rec.partner_id.legal_name
+            rec.cnpj_cpf = rec.partner_id.cnpj_cpf
+            rec.inscr_est = rec.partner_id.inscr_est
+            rec.inscr_mun = rec.partner_id.inscr_mun
+            rec.suframa = rec.partner_id.suframa
 
     def _set_br_suframa(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.suframa = self.suframa
+        for rec in self:
+            rec.partner_id.suframa = rec.suframa
 
     def _set_br_legal_name(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.legal_name = self.legal_name
+        for rec in self:
+            rec.partner_id.legal_name = rec.legal_name
 
     def _set_br_cnpj_cpf(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.cnpj_cpf = self.cnpj_cpf
+        for rec in self:
+            rec.partner_id.cnpj_cpf = rec.cnpj_cpf
 
     def _set_br_inscr_est(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.inscr_est = self.inscr_est
+        for rec in self:
+            rec.partner_id.inscr_est = rec.inscr_est
 
     def _set_br_inscr_mun(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.inscr_mun = self.inscr_mun
+        for rec in self:
+            rec.partner_id.inscr_mun = rec.inscr_mun
 
     def _set_br_number(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.number = self.number
+        for rec in self:
+            rec.partner_id.number = rec.number
 
     def _set_br_district(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.district = self.district
+        for rec in self:
+            rec.partner_id.district = rec.district
 
     def _set_city_id(self):
         """ Write the l10n_br specific functional fields. """
-        self.partner_id.city_id = self.city_id
+        for rec in self:
+            rec.partner_id.city_id = rec.city_id
 
     def _compute_expiry_date(self):
-        try:
-            pfx = base64.decodestring(
-                self.with_context(bin_size=False).nfe_a1_file)
-            pfx = crypto.load_pkcs12(pfx, self.nfe_a1_password)
-            cert = pfx.get_certificate()
-            end = datetime.strptime(
-                cert.get_notAfter().decode(), '%Y%m%d%H%M%SZ')
-            subj = cert.get_subject()
-            self.cert_expire_date = end.date()
-            if datetime.now() < end:
-                self.cert_state = 'valid'
-            else:
-                self.cert_state = 'expired'
-            self.cert_information = "%s\n%s\n%s\n%s" % (
-                subj.CN, subj.L, subj.O, subj.OU)
-        except crypto.Error:
-            self.cert_state = 'invalid_password'
-        except:
-            self.cert_state = 'unknown'
-            _logger.warning(
-                _(u'Unknown error when validating certificate'),
-                exc_info=True)
+        for rec in self:
+            try:
+                pfx = base64.decodestring(
+                    rec.with_context(bin_size=False).nfe_a1_file)
+                pfx = crypto.load_pkcs12(pfx, rec.nfe_a1_password)
+                cert = pfx.get_certificate()
+                end = datetime.strptime(
+                    cert.get_notAfter().decode(), '%Y%m%d%H%M%SZ')
+                subj = cert.get_subject()
+                rec.cert_expire_date = end.date()
+                if datetime.now() < end:
+                    rec.cert_state = 'valid'
+                else:
+                    rec.cert_state = 'expired'
+                rec.cert_information = "%s\n%s\n%s\n%s" % (
+                    subj.CN, subj.L, subj.O, subj.OU)
+            except crypto.Error:
+                rec.cert_state = 'invalid_password'
+            except Exception as exc:
+                rec.cert_state = 'unknown'
+                _logger.warning(
+                    _('Unknown error when validating certificate. %s', exc),
+                    exc_info=True)
 
     cnpj_cpf = fields.Char(
         compute=_get_br_data, inverse=_set_br_cnpj_cpf, size=18,
-        string=u'CNPJ')
+        string=_("CNPJ"))
 
     inscr_est = fields.Char(
         compute=_get_br_data, inverse=_set_br_inscr_est, size=16,
-        string=u'State Inscription')
+        string=_("State Inscription"))
 
     inscr_mun = fields.Char(
         compute=_get_br_data, inverse=_set_br_inscr_mun, size=18,
-        string=u'Municipal Inscription')
+        string=_("Municipal Inscription"))
 
     suframa = fields.Char(
         compute=_get_br_data, inverse=_set_br_suframa, size=18,
-        string=u'Suframa')
+        string=_("Suframa"))
 
     legal_name = fields.Char(
         compute=_get_br_data, inverse=_set_br_legal_name, size=128,
-        string=u'Legal Name')
+        string=_("Legal Name"))
 
     city_id = fields.Many2one(
         compute=_get_address_data, inverse='_set_city_id',
-        comodel_name='res.state.city', string="City", multi='address')
+        comodel_name='res.state.city', string=_("City ID"), multi='address')
 
     district = fields.Char(
         compute=_get_address_data, inverse='_set_br_district', size=32,
-        string=u"District", multi='address')
+        string=_("District"), multi='address')
 
     number = fields.Char(
         compute=_get_address_data, inverse='_set_br_number', size=10,
-        string=u"Number", multi='address')
+        string=_("Number"), multi='address')
 
-    nfe_a1_file = fields.Binary(u'NFe A1 File')
-    nfe_a1_password = fields.Char(u'NFe A1 Password', size=64)
+    nfe_a1_file = fields.Binary(_("NFe A1 File"))
+    nfe_a1_password = fields.Char(_("NFe A1 Password"), size=64)
 
     cert_state = fields.Selection(
-        [('not_loaded', u'Not loaded'),
-         ('expired', u'Expired'),
-         ('invalid_password', u'Invalid Password'),
-         ('unknown', u'Unknown'),
-         ('valid', u'Valid')],
-        string=u"Cert. State", compute=_compute_expiry_date,
+        [('not_loaded', _("Not loaded")),
+         ('expired',  _("Expired")),
+         ('invalid_password', _("Invalid Password")),
+         ('unknown', _("Unknown")),
+         ('valid', _("Valid"))],
+        string=_("Cet. State"), compute=_compute_expiry_date,
         default='not_loaded')
     cert_information = fields.Text(
-        string=u"Cert. Info", compute=_compute_expiry_date)
+        string=_("Cert. Info"), compute=_compute_expiry_date)
     cert_expire_date = fields.Date(
-        string=u"Cert. Expiration Date", compute=_compute_expiry_date)
+        string=_("Cert. Expiration Date"), compute=_compute_expiry_date)
 
     @api.onchange('cnpj_cpf')
     def onchange_mask_cnpj_cpf(self):
-        if self.cnpj_cpf:
-            val = re.sub('[^0-9]', '', self.cnpj_cpf)
-            if len(val) == 14:
-                cnpj_cpf = "%s.%s.%s/%s-%s"\
-                    % (val[0:2], val[2:5], val[5:8], val[8:12], val[12:14])
-                self.cnpj_cpf = cnpj_cpf
+        for rec in self:
+            if rec.cnpj_cpf:
+                val = re.sub('[^0-9]', '', rec.cnpj_cpf)
+                if len(val) == 14:
+                    cnpj_cpf = "%s.%s.%s/%s-%s"\
+                        % (val[0:2], val[2:5], val[5:8], val[8:12], val[12:14])
+                    rec.cnpj_cpf = cnpj_cpf
 
     @api.onchange('city_id')
     def onchange_city_id(self):
@@ -157,13 +169,15 @@ class ResCompany(models.Model):
         para manter a compatibilidade entre os demais módulos que usam o
         campo city.
         """
-        if self.city_id:
-            self.city = self.city_id.name
+        for rec in self:
+            if rec.city_id:
+                rec.city = rec.city_id.name
 
     @api.onchange('zip')
     def onchange_mask_zip(self):
-        if self.zip:
-            val = re.sub('[^0-9]', '', self.zip)
-            if len(val) == 8:
-                zip = "%s-%s" % (val[0:5], val[5:8])
-                self.zip = zip
+        for rec in self:
+            if rec.zip:
+                val = re.sub('[^0-9]', '', rec.zip)
+                if len(val) == 8:
+                    zip = "%s-%s" % (val[0:5], val[5:8])
+                    rec.zip = zip
